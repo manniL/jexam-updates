0dff9c16cbc9f222fab389697b7f8142920eed68
Alexander Lichter <manniL@gmx.net>
2017-08-29 11:25:53 +0200

    Implement basic jExam checker
    + Well, that was fast 🎉

---
+++ src/Feed.js
@@ -0,0 +1,14 @@
+import Config from '../config.json'
+import axios from 'axios'
+
+export default class Feed {
+  static async load (feedURL) {
+    const apiKey = Config.rss2json
+    try {
+      const response = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(feedURL) + '&api_key=' + apiKey)
+      return response.data
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,64 @@
+import Twitter from 'twit'
+import Feed from './Feed'
+import Config from '../config.json'
+import fs from 'fs-extra'
+import { existsSync } from 'fs'
+import { isEqual } from 'lodash'
+
+const twitterClient = new Twitter(Config.twitter)
+const feedUrl = 'http://feeds.feedburner.com/jExam'
+
+const handleData = async function (newData) {
+  const lastData = await getLastData()
+  if (!isEqual(lastData, newData)) {
+    await writeToLast(newData) //Don't wait for write op here
+    if (newData.title.includes('Prüfungsergebnisse')) {
+      twitterChanges()
+    }
+  } else {
+    console.log('No changes')
+  }
+}
+
+const writeToLast = async function (jsonObject) {
+  fs.writeJson('last.json', jsonObject)
+    .then(() => console.log('SUCCESS WRITING of ' + JSON.stringify(jsonObject)))
+    .catch((err) => console.error(`Failed: ${err}`))
+}
+
+const twitterChanges = () => {
+  console.log('Results have been updated! Tweeting now')
+
+  const random = Math.random().toString(36).slice(2, 6)
+  twitterClient
+    .post('statuses/update', {
+      status: 'Beep boop, jExam result have been updated! To see what has' +
+      ' been changed, visit https://jexam.inf.tu-dresden.de/ | ' + random
+    })
+    .catch((e) => console.log('Failed with ' + e))
+    .then((resp) => {
+      if (resp.data.errors) {
+        console.log(resp.data.errors)
+        throw new Error('Tweeting failed')
+      } else {
+        console.log('Tweeted successfully')
+      }
+    })
+
+}
+
+const getLastData = async () => {
+  if (!existsSync('last.json')) {
+    await writeToLast({})
+  }
+  try {
+    return fs.readJson('last.json')
+  } catch (e) { console.log(e)}
+}
+
+async function hereWeGo () {
+  const data = (await Feed.load(feedUrl)).items[0]
+  await handleData(data)
+}
+
+hereWeGo().then(() => console.log('Done checking'))
